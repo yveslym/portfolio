8862f87e3ab11c47364ad341d7b5aa11ed660155
yves songolo <yves.songolo@gmail.com>
2017-10-20 13:05:50 -0700

    fix the ui, post and teach request

---
+++ MOB1/gradeBook.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Student{
+    
+    var name: String?
+    var grade: String?
+    
+}
+
+class ClassRoom{
+    var students:[Student]?
+}
